3b9b2d22ffb9e5b1744226abf269d198daf57d3f
yoursmukund <mukundsharma@outlook.com>
2018-01-05 22:27:09 +0530

    Update areaWidget.jsx

--- src/areaWidget/areaWidget.jsx
+++ src/areaWidget/areaWidget.jsx
@@ -12,7 +12,7 @@ class AreaWidget extends Component {
     return (
       <div style={{ background: '#ECECEC', padding: '20px' }}>
         <Card bordered={false} style={{ width: '340px', height: '140px', textAlign: 'left', borderRadius: 10 }}>
-          <div style={{fontWeight: 'bold', color: '#4c5e70', fontSize: 12, marginTop: 5}}>Cluster-wide Controller IO B/W</div>
+          <div style={{fontWeight: 'bold', color: '#4c5e70', fontSize: 12, marginTop: 5}}>ljlkj</div>
           <div style={{fontWeight: 'SemiBold', color: '#7b8da0', fontSize: 12}}>{this.props.data[0].name}</div>
           <AreaChart width={300} height={90} data={this.props.data}
                 margin={{top: 0, right: 15, left: 15, bottom: 0}}>
